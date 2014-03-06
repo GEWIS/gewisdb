6894eb0c5d2b5fb113a99da892a866890720e5e9
Pieter Kokx <pieter@kokx.nl>
2014-03-06 21:40:05 +0100

    Removed translation stuff.
    We won't be needing this here.

--- module/Application/config/module.config.php
+++ module/Application/config/module.config.php
@@ -56,20 +56,7 @@ return array(
         'abstract_factories' => array(
             'Zend\Cache\Service\StorageCacheAbstractServiceFactory',
             'Zend\Log\LoggerAbstractServiceFactory',
-        ),
-        'aliases' => array(
-            'translator' => 'MvcTranslator',
-        ),
-    ),
-    'translator' => array(
-        'locale' => 'en_US',
-        'translation_file_patterns' => array(
-            array(
-                'type'     => 'gettext',
-                'base_dir' => __DIR__ . '/../language',
-                'pattern'  => '%s.mo',
-            ),
-        ),
+        )
     ),
     'controllers' => array(
         'invokables' => array(
