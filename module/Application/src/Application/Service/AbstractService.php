dd77bd13c328d4eea5680559a5f612529bd08348
Pieter Kokx <pieter@kokx.nl>
2014-03-08 23:15:16 +0100

    Created abstract service.

---
+++ module/Application/src/Application/Service/AbstractService.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Application\Service;
+
+use Zend\ServiceManager\ServiceManager;
+use Zend\ServiceManager\ServiceManagerAwareInterface;
+
+abstract class AbstractService implements ServiceManagerAwareInterface
+{
+
+    /**
+     * Service manager.
+     *
+     * @var ServiceManager
+     */
+    protected $sm;
+
+    /**
+     * Set the service manager.
+     *
+     * @param ServiceManager $sm
+     */
+    public function setServiceManager(ServiceManager $sm)
+    {
+        $this->sm = $sm;
+    }
+
+    /**
+     * Get the service manager.
+     *
+     * @return ServiceManager
+     */
+    public function getServiceManager()
+    {
+        return $this->sm;
+    }
+}
