353e1d09fcdf2b83a2fa9e3f78d67a4a1102eb00
Pieter Kokx <pieter@kokx.nl>
2014-03-08 23:59:58 +0100

    Created meeting mapper.

---
+++ module/Database/src/Database/Mapper/Meeting.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Database\Mapper;
+
+use Database\Model\Meeting as MeetingModel;
+use Doctrine\ORM\EntityManager;
+
+class Meeting
+{
+
+    /**
+     * Doctrine entity manager.
+     *
+     * @var EntityManager
+     */
+    protected $em;
+
+
+    /**
+     * Constructor
+     *
+     * @param EntityManager $em
+     */
+    public function __construct(EntityManager $em)
+    {
+        $this->em = $em;
+    }
+
+    /**
+     * Persist a meeting model.
+     *
+     * @param MeetingModel $meeting Meeting to persist.
+     */
+    public function persist(MeetingModel $meeting)
+    {
+        $this->em->persist($meeting);
+        $this->em->flush();
+    }
+
+    /**
+     * Get the repository for this mapper.
+     *
+     * @return Doctrine\ORM\EntityRepository
+     */
+    public function getRepository()
+    {
+        return $this->em->getRepository('Database\Model\Meeting');
+    }
+
+}
