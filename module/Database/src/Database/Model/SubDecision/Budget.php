81aabb25cd95d1a0709b0637a141b3b6c400cbae
Pieter Kokx <pieter@kokx.nl>
2014-03-16 00:44:54 +0100

    Added budget specific fields.

--- module/Database/src/Database/Model/SubDecision/Budget.php
+++ module/Database/src/Database/Model/SubDecision/Budget.php
@@ -22,6 +22,40 @@ class Budget extends SubDecision
      */
     protected $author;
 
+    /**
+     * Name of the budget.
+     *
+     * @ORM\Column(type="string")
+     */
+    protected $name;
+
+    /**
+     * Version of the budget.
+     *
+     * @ORM\Column(type="string",length=32)
+     */
+    protected $version;
+
+    /**
+     * Date of the budget.
+     *
+     * @ORM\Column(type="date")
+     */
+    protected $date;
+
+    /**
+     * If the budget was approved.
+     *
+     * @ORM\Column(type="boolean")
+     */
+    protected $approval;
+
+    /**
+     * If there were changes made.
+     *
+     * @ORM\Column(type="boolean")
+     */
+    protected $changes;
 
     /**
      * Get the author.
@@ -41,5 +75,105 @@ class Budget extends SubDecision
     public function setAuthor(Member $author)
     {
         $this->author = $author;
+    }
+
+    /**
+     * Get the name.
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set the name.
+     *
+     * @param string $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * Get the version.
+     *
+     * @return string
+     */
+    public function getVersion()
+    {
+        return $this->version;
+    }
+
+    /**
+     * Set the version.
+     *
+     * @param string $version
+     */
+    public function setVersion($version)
+    {
+        $this->version = $version;
+    }
+
+    /**
+     * Get the date.
+     *
+     * @return \DateTime
+     */
+    public function getDate()
+    {
+        return $this->date;
+    }
+
+    /**
+     * Set the date.
+     *
+     * @param \DateTime $date
+     */
+    public function setDate(\DateTime $date)
+    {
+        $this->date = $date;
+    }
+
+    /**
+     * Get approval status.
+     *
+     * @return bool
+     */
+    public function getApproval()
+    {
+        return $this->approval;
+    }
+
+    /**
+     * Set approval status.
+     *
+     * @param bool $approval
+     */
+    public function setApproval($approval)
+    {
+        $this->approval = $approval;
+    }
+
+    /**
+     * Get if changes were made.
+     *
+     * @return bool
+     */
+    public function getChanges()
+    {
+        return $this->changes;
+    }
+
+    /**
+     * Set if changes were made.
+     *
+     * @param bool $changes
+     */
+    public function setChanges($changes)
+    {
+        $this->changes = $changes;
     }
 }
