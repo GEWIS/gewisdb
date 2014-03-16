71765963cce817d82b479bbbe1a306e201ea31e2
Pieter Kokx <pieter@kokx.nl>
2014-03-16 01:03:14 +0100

    Added reference to organ foundation.

--- module/Database/src/Database/Model/SubDecision/Abrogation.php
+++ module/Database/src/Database/Model/SubDecision/Abrogation.php
@@ -15,6 +15,41 @@ class Abrogation extends SubDecision
 {
 
     /**
+     * Reference to foundation of organ.
+     *
+     * @ORM\ManyToOne(targetEntity="Foundation")
+     * @ORM\JoinColumns({
+     *  @ORM\JoinColumn(name="r_meeting_type", referencedColumnName="meeting_type"),
+     *  @ORM\JoinColumn(name="r_meeting_number", referencedColumnName="meeting_number"),
+     *  @ORM\JoinColumn(name="r_decision_point", referencedColumnName="decision_point"),
+     *  @ORM\JoinColumn(name="r_decision_number", referencedColumnName="decision_number"),
+     *  @ORM\JoinColumn(name="r_number", referencedColumnName="number")
+     * })
+     */
+    protected $foundation;
+
+
+    /**
+     * Get organ foundation.
+     *
+     * @return Foundation
+     */
+    public function getFoundation()
+    {
+        return $this->foundation;
+    }
+
+    /**
+     * Set organ foundation.
+     *
+     * @param Foundation $foundation
+     */
+    public function setFoundation($foundation)
+    {
+        $this->foundation = $foundation;
+    }
+
+    /**
      * Get the content.
      *
      * @todo implement this
