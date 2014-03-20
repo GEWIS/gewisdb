1376b5b138f1c62c8a826b3e2178c2e1c7c546d2
Pieter Kokx <pieter@kokx.nl>
2014-03-20 15:31:27 +0100

    Added fieldset for addresses.

---
+++ module/Database/src/Database/Form/Fieldset/Address.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Database\Form\Fieldset;
+
+use Zend\Form\Fieldset;
+use Zend\InputFilter\InputFilterProviderInterface;
+
+class Address extends Fieldset
+    implements InputFilterProviderInterface
+{
+
+    public function __construct()
+    {
+        parent::__construct('address');
+
+        $this->add(array(
+            'name' => 'type',
+            'type' => 'hidden'
+        ));
+
+        $this->add(array(
+            'name' => 'country',
+            'type' => 'text',
+            'options' => array(
+                'label' => 'Land',
+                'value' => 'netherlands'
+            )
+        ));
+
+        $this->add(array(
+            'name' => 'street',
+            'type' => 'text',
+            'options' => array(
+                'label' => 'Straat'
+            )
+        ));
+
+        $this->add(array(
+            'name' => 'number',
+            'type' => 'text',
+            'options' => array(
+                'label' => 'Huisnummer'
+            )
+        ));
+
+        $this->add(array(
+            'name' => 'postalCode',
+            'type' => 'text',
+            'options' => array(
+                'label' => 'Postcode'
+            )
+        ));
+
+        $this->add(array(
+            'name' => 'City',
+            'type' => 'text',
+            'options' => array(
+                'label' => 'Stad'
+            )
+        ));
+
+        $this->add(array(
+            'name' => 'phone',
+            'type' => 'text',
+            'options' => array(
+                'label' => 'Telefoonnummer'
+            )
+        ));
+
+        // TODO: filters
+    }
+}
