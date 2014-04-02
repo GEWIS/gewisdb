2f35de57636fc6450595a7c81e7ae4988b871221
Pieter Kokx <pieter@kokx.nl>
2014-04-02 20:53:49 +0200

    Added check for abrogated organs.

--- module/Database/src/Database/Mapper/Meeting.php
+++ module/Database/src/Database/Mapper/Meeting.php
@@ -90,6 +90,12 @@ class Meeting
     /**
      * Search for organ decisions.
      *
+     * This is a really complicated query, we might want to create a
+     * materialized view for organs, with a field if they are abrogated or not.
+     *
+     * And since events are implemented anyways, we might want to use that to
+     * automatically process changes.
+     *
      * @param string $query
      *
      * @return array Organs
@@ -104,6 +110,22 @@ class Meeting
             ->orWhere('LOWER(o.abbr) LIKE :name')
             ->join('o.decision', 'd')
             ->join('d.meeting', 'm');
+
+        // we want to leave out organs that have been abrogated
+        $qbn = $this->em->createQueryBuilder();
+        $qbn->select('a')
+            ->from('Database\Model\SubDecision\Abrogation', 'a')
+            ->join('a.foundation', 'x')
+            ->where('x.meeting_type = o.meeting_type')
+            ->andWhere('x.meeting_number = o.meeting_number')
+            ->andWhere('x.decision_point = o.decision_point')
+            ->andWhere('x.decision_number = o.decision_number');
+        // add the subexpression
+        $qb->andWhere($qb->expr()->not(
+            $qb->expr()->exists(
+                $qbn->getDql()
+            )
+        ));
 
         $qb->setParameter(':name', '%' . strtolower($query) . '%');
 
