8e31d46c2cfb2b0a406f700d5b65f957cccb185d
Pieter Kokx <pieter@kokx.nl>
2014-04-19 12:06:58 +0200

    Added fieldset for subdecisions.

---
+++ module/Database/src/Database/Form/Fieldset/SubDecision.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace Database\Form\Fieldset;
+
+use Zend\Form\Fieldset;
+use Zend\InputFilter\InputFilterProviderInterface;
+
+use Database\Model\SubDecision as SubDecisionModel;
+use Database\Model\Meeting;
+
+class SubDecision extends Fieldset
+    implements InputFilterProviderInterface
+{
+
+    public function __construct()
+    {
+        parent::__construct('subdecision');
+
+        $this->add(array(
+            'name' => 'meeting_type',
+            'type' => 'hidden'
+        ));
+
+        $this->add(array(
+            'name' => 'meeting_number',
+            'type' => 'hidden'
+        ));
+
+        $this->add(array(
+            'name' => 'decision_point',
+            'type' => 'hidden'
+        ));
+
+        $this->add(array(
+            'name' => 'decision_number',
+            'type' => 'hidden'
+        ));
+
+        $this->add(array(
+            'name' => 'number',
+            'type' => 'hidden'
+        ));
+    }
+
+    /**
+     * Specification for input filters.
+     */
+    public function getInputFilterSpecification()
+    {
+        return array(
+            'meeting_type' => array(
+                'required' => true,
+                'validators' => array(
+                    array(
+                        'name' => 'in_array',
+                        'options' => array(
+                            'haystack' => Meeting::getTypes()
+                        )
+                    )
+                )
+            ),
+            'meeting_number' => array(
+                'required' => true,
+                'validators' => array(
+                    array('name' => 'digits')
+                )
+            ),
+            'decision_point' => array(
+                'required' => true,
+                'validators' => array(
+                    array('name' => 'digits')
+                )
+            ),
+            'decision_number' => array(
+                'required' => true,
+                'validators' => array(
+                    array('name' => 'digits')
+                )
+            ),
+            'number' => array(
+                'required' => true,
+                'validators' => array(
+                    array('name' => 'digits')
+                )
+            ),
+        );
+    }
+}
