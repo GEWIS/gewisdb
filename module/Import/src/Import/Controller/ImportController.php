4215053e027039583cc1b84096375478a8797576
Pieter Kokx <pieter@kokx.nl>
2014-06-02 00:23:56 +0200

    Added initial old database reading code.

--- module/Import/src/Import/Controller/ImportController.php
+++ module/Import/src/Import/Controller/ImportController.php
@@ -17,7 +17,43 @@ class ImportController extends AbstractActionController
     {
         $console = $this->getConsole();
 
-        $conn = $this->getServiceLocator()->get('doctrine.connection.orm_import');
+        $db = $this->getServiceLocator()->get('doctrine.connection.orm_import');
+
+        // TODO: save this somewhere
+        $lastDate = '1900-01-01';
+
+        $stmt = $db->prepare("SELECT v.*, t.* FROM vergadering AS v
+            INNER JOIN vergadertype AS t ON (v.vergadertypeid = t.vergadertypeid)
+            WHERE v.datum >= ?
+            ORDER BY v.datum");
+        $stmt->bindParam(1, $lastDate);
+        $stmt->execute();
+
+        // statement to get all decisions
+        $dStmt = $db->prepare("SELECT b.* FROM besluit AS b
+            WHERE b.vergadertypeid = :type AND b.vergadernr = :nr
+            ORDER BY puntnr ASC, besluitnr ASC");
+
+        while (($vergadering = $stmt->fetch()) != null) {
+            echo $vergadering['vergaderafk'] . ' ' . $vergadering['vergadernr'] . " ( " . $vergadering['datum'] . "):\n";
+            echo "-----------------------------------------\n";
+
+            $dStmt->execute(array(
+                'type' => $vergadering['vergadertypeid'],
+                'nr' => $vergadering['vergadernr']
+            ));
+
+            $rows = $dStmt->fetchAll();
+
+            if (empty($rows)) {
+                continue;
+            }
+
+            var_dump($rows);
+
+            echo "-----------------------------------------\n";
+            sleep(2);
+        }
     }
 
     /**
