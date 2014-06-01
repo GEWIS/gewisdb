6aff95ce390b1466c069811634656c51e22e6002
Pieter Kokx <pieter@kokx.nl>
2014-06-01 23:22:17 +0200

    Added code for opening database connection.

--- module/Import/src/Import/Controller/ImportController.php
+++ module/Import/src/Import/Controller/ImportController.php
@@ -4,6 +4,9 @@ namespace Import\Controller;
 
 use Zend\Mvc\Controller\AbstractActionController;
 
+use Doctrine\DBAL\Configuration;
+use Doctrine\DBAL\DriverManager;
+
 class ImportController extends AbstractActionController
 {
 
@@ -12,6 +15,52 @@ class ImportController extends AbstractActionController
      */
     public function importAction()
     {
-        echo "Importing.......\n";
+        $console = $this->getConsole();
+
+        /*
+         * Read database connection info.
+         */
+
+        echo "Enter database information:\n";
+
+        echo "Database name: ";
+        $db = $console->readLine();
+
+        echo "Database host [localhost]: ";
+        $host = $console->readLine();
+        if (empty($host)) {
+            $host = 'localhost';
+        }
+
+        echo "Database username: ";
+        $username = $console->readLine();
+
+        echo "Database password: ";
+        // BEWARE: ugly hack to hide password entry
+        system('stty -echo');
+        $password = $console->readLine();
+        system('stty echo');
+        echo "\n";
+
+        /*
+         * Open database connection.
+         */
+        $info = array(
+            'dbname' => $db,
+            'user' => $username,
+            'password' => $password,
+            'host' => $host,
+            'driver' => 'pdo_pgsql'
+        );
+
+        $conn = DriverManager::getConnection($info, new Configuration());
+    }
+
+    /**
+     * Get the console object.
+     */
+    protected function getConsole()
+    {
+        return $this->getServiceLocator()->get('console');
     }
 }
