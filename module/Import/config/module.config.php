2e18be2fe9b0c051637ae707b7fa5a03e7fdedb3
Pieter Kokx <pieter@kokx.nl>
2014-06-06 13:19:25 +0200

    Removed entities config for Import module.

--- module/Import/config/module.config.php
+++ module/Import/config/module.config.php
@@ -5,20 +5,6 @@ return array(
             'Import\Controller\Import' => 'Import\Controller\ImportController'
         )
     ),
-    'doctrine' => array(
-        'driver' => array(
-            'database_entities' => array(
-                'class' => 'Doctrine\ORM\Mapping\Driver\AnnotationDriver',
-                'cache' => 'array',
-                'paths' => array(__DIR__ . '/../src/Import/Model/')
-            ),
-            'orm_default' => array(
-                'drivers' => array(
-                    'Import\Model' => 'database_entities'
-                )
-            )
-        )
-    ),
     'console' => array(
         'router' => array(
             'routes' => array(
