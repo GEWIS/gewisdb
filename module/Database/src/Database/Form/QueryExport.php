facdbbdf47d9d253b10a8d36c4b34a7055ae0469
Pieter Kokx <pieter@kokx.nl>
2014-12-13 19:34:24 +0100

    Refactored the QueryExport form.

--- module/Database/src/Database/Form/QueryExport.php
+++ module/Database/src/Database/Form/QueryExport.php
@@ -2,25 +2,13 @@
 
 namespace Database\Form;
 
-use Zend\Form\Form;
-use Zend\InputFilter\InputFilter;
-use Zend\InputFilter\InputFilterProviderInterface;
 
-class QueryExport extends Form
-    implements InputFilterProviderInterface
+class QueryExport extends Query
 {
 
     public function __construct()
     {
         parent::__construct();
-
-        $this->add(array(
-            'name' => 'query',
-            'type' => 'hidden',
-            'options' => array(
-                'label' => 'Query input'
-            )
-        ));
 
         $this->add(array(
             'name' => 'type',
@@ -32,22 +20,7 @@ class QueryExport extends Form
             )
         ));
 
-        $this->add(array(
-            'name' => 'submit',
-            'type' => 'submit',
-            'attributes' => array(
-                'value' => 'Export'
-            )
-        ));
+        $this->get('submit')->setAttribute('value', 'export');
         $this->get('submit')->setLabel('Export');
-    }
-
-    /**
-     * Specification of input filter.
-     */
-    public function getInputFilterSpecification()
-    {
-        return array(
-        );
     }
 }
