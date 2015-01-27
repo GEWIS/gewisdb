6f4e04224cbb73aa0c22b594133acd19b015e619
Pieter Kokx <pieter@kokx.nl>
2015-01-27 16:29:08 +0100

    Added basic member data.

--- module/Export/src/Export/Query/Member.php
+++ module/Export/src/Export/Query/Member.php
@@ -17,6 +17,43 @@ class Member
      */
     protected $conn;
 
+    /**
+     * Member exists statement.
+     */
+    protected $existsStmt;
+
+
+    /**
+     * Prepare the member exists statement.
+     */
+    public function getExistsStmt()
+    {
+        if (null === $this->existsStmt) {
+            $this->existsStmt = $this->getConnection()->prepare("
+                SELECT m.lidnummer FROM gewis_lid AS m
+                WHERE m.lidnummer = :lidnr");
+        }
+        return $this->existsStmt;
+    }
+
+
+    /**
+     * Check if the member exists.
+     *
+     * @param int $lidnr
+     *
+     * @return boolean If exists
+     */
+    public function checkMemberExists($lidnr)
+    {
+        $stmt = $this->getExistsStmt();
+
+        $stmt->execute(array(
+            'lidnr' => $lidnr
+        ));
+
+        return count($stmt->fetchAll()) == 1;
+    }
 
     public function setConnection(Connection $conn)
     {
--- module/Export/src/Export/Service/Member.php
+++ module/Export/src/Export/Service/Member.php
@@ -4,6 +4,9 @@ namespace Export\Service;
 
 use Application\Service\AbstractService;
 
+use Database\Model\Member as MemberModel;
+use Database\Model\Address;
+
 class Member extends AbstractService
 {
 
@@ -15,8 +18,82 @@ class Member extends AbstractService
         $mapper = $this->getMemberMapper();
 
         foreach ($mapper->findAll() as $member) {
-            echo "Exporting " . $member->getFullName() . "\n";
+            //echo "Exporting " . $member->getFullName() . "\n";
             // TODO: export member
+
+            $haddr = array(
+                'straat' => '',
+                'postcode' => '',
+                'plaats' => '',
+                'telefoon' => ''
+            );
+            $kaddr = array(
+                'straat' => '',
+                'postcode' => '',
+                'plaats' => '',
+                'telefoon' => ''
+            );
+
+            foreach ($member->getAddresses() as $address) {
+                if ($address->getType() == Address::TYPE_HOME) {
+                    $haddr['straat'] = $address->getStreet();
+                    $haddr['postcode'] = $address->getPostalCode();
+                    $haddr['plaats'] = $address->getCity();
+                    $haddr['telefoon'] = $address->getPhone();
+                } else if ($address->getType() == Address::TYPE_STUDENT) {
+                    $kaddr['straat'] = $address->getStreet();
+                    $kaddr['postcode'] = $address->getPostalCode();
+                    $kaddr['plaats'] = $address->getCity();
+                    $kaddr['telefoon'] = $address->getPhone();
+                }
+            }
+
+            $data = array(
+                'lidnummer' => $member->getLidnr(),
+                'achternaam' => $member->getLastName(),
+                'tussen' => $member->getMiddleName(),
+                'voorlet' => $member->getInitials(),
+                'voornaam' => $member->getFirstName(),
+                'gesl' => $member->getGender() == MemberModel::GENDER_MALE ? '1': '0',
+                'generatie' => $member->getGeneration(),
+                'e_mail' => $member->getEmail(),
+                // TODO 'betaald' => 'TODO',
+                // TODO 'lidsoortid' => 'TODO',
+                'verloopdatum' => $member->getExpiration()->format('Y-m-d'),
+                'geboortedatum' => $member->getExpiration()->format('Y-m-d'),
+                // thuis
+                'hstraat' => $haddr['straat'],
+                'hpostcode' => $haddr['postcode'],
+                'hplaats' => $haddr['plaats'],
+                'htelefoon' => $haddr['telefoon'],
+                // kamer
+                'kstraat' => $kaddr['straat'],
+                'kpostcode' => $kaddr['postcode'],
+                'kplaats' => $kaddr['plaats'],
+                'ktelefoon' => $kaddr['telefoon'],
+                // mail settings
+                // TODO 'direct' => 'TODO',
+                // TODO 'plijst' => 'TODO',
+                // TODO 'winlijst' => 'TODO',
+                // TODO 'gewislijst' => 'TODO',
+                // TODO 'vacature' => 'TODO',
+                // TODO 'babbel' => 'TODO',
+            );
+
+            $this->saveMember($data);
+        }
+    }
+
+    /**
+     * Save an old member.
+     *
+     * @param $data Data to save
+     */
+    public function saveMember($data)
+    {
+        // first check if it is an existing member
+        if ($this->getQuery()->checkMemberExists($data['lidnummer'])) {
+            echo "Lid " . $data['lidnummer'] . " bestaat al.\n";
         }
     }
 
