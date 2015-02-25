45b6e72fae85cd207a81077426ea224543df3acf
Pieter Kokx <pieter@kokx.nl>
2015-02-25 15:46:46 +0100

    Added new doctrine connection.

--- module/Report/config/module.config.php
+++ module/Report/config/module.config.php
@@ -5,6 +5,54 @@ return array(
             //'Export\Controller\Export' => 'Export\Controller\ExportController'
         )
     ),
+    'doctrine' => array(
+        'connection' => array(
+            'orm_report' => array(),
+        ),
+        'configuration' => array(
+            'orm_report' => array(
+                'metadata_cache' => 'array',
+                'query_cache' => 'array',
+                'result_cache' => 'array',
+                'hydration_cache' => 'array',
+                'driver' => 'orm_report',
+                'generate_proxies' => true,
+                'proxy_dir' => 'data/DoctrineORMModule/Proxy',
+                'proxy_namespace' => 'DoctrineORMModule\Proxy',
+                'filters' => array()
+            )
+        ),
+        'driver' => array(
+            'Report_Driver' => array(
+                'class' => 'Doctrine\ORM\Mapping\Driver\AnnotationDriver',
+                'cache' => 'array',
+                'paths' => array(
+                    __DIR__ . '/../src/Report/Model'
+                )
+            ),
+            'orm_driver' => array(
+                'class' => 'Doctrine\ORM\Mapping\Driver\DriverChain',
+                'drivers' => array(
+                    'Report\Model' => 'Report_Driver'
+                )
+            )
+        ),
+        'entitymanager' => array(
+            'orm_report' => array(
+                'connection' => 'orm_report',
+                'configuration' => 'orm_report'
+            )
+        )
+        'eventmanager' => array(
+            'orm_report' => array()
+        ),
+        'sql_logger_collector' => array(
+            'orm_report' => array()
+        ),
+        'entity_resolver' => array(
+            'orm_report' => array()
+        )
+    ),
     'console' => array(
         'router' => array(
             'routes' => array(
