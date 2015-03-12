faae11b4b20c66be08e03c07982afd73604f3036
Pieter Kokx <pieter@kokx.nl>
2015-03-12 12:56:07 +0100

    Created basic reports data model.

---
+++ module/Report/src/Report/Model/Organ.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace Report\Model;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Organ entity.
+ *
+ * Note that this entity is derived from the decisions themself.
+ *
+ * @ORM\Entity
+ */
+class Organ
+{
+
+    const ORGAN_TYPE_COMMITTEE = 'committee';
+    const ORGAN_TYPE_AV_COMMITTEE = 'avc';
+    const ORGAN_TYPE_FRATERNITY = 'fraternity';
+
+    /**
+     * Id.
+     *
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue
+     */
+    protected $id;
+
+    /**
+     * Abbreviation (only for when organs are created)
+     *
+     * @ORM\Column(type="string")
+     */
+    protected $abbr;
+
+    /**
+     * Name (only for when organs are created)
+     *
+     * @ORM\Column(type="string")
+     */
+    protected $name;
+
+    /**
+     * Type of the organ.
+     *
+     * @ORM\Column(type="string")
+     */
+    protected $type;
+
+    /**
+     * Reference to foundation of organ.
+     *
+     * @ORM\OneToOne(targetEntity="Report\Model\SubDecision\Foundation")
+     * @ORM\JoinColumns({
+     *  @ORM\JoinColumn(name="r_meeting_type", referencedColumnName="meeting_type"),
+     *  @ORM\JoinColumn(name="r_meeting_number", referencedColumnName="meeting_number"),
+     *  @ORM\JoinColumn(name="r_decision_point", referencedColumnName="decision_point"),
+     *  @ORM\JoinColumn(name="r_decision_number", referencedColumnName="decision_number"),
+     *  @ORM\JoinColumn(name="r_number", referencedColumnName="number")
+     * })
+     */
+    protected $foundation;
+
+    /**
+     * Foundation date.
+     *
+     * @ORM\Column(type="date")
+     */
+    protected $foundationDate;
+
+    /**
+     * Abrogation date.
+     *
+     * @ORM\Column(type="date")
+     */
+    protected $abrogationDate;
+}
---
+++ module/Report/src/Report/Model/OrganMember.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Report\Model;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Organ member entity.
+ *
+ * Note that this entity is derived from the decisions themself.
+ *
+ * @ORM\Entity
+ */
+class OrganMember
+{
+
+    /**
+     * Organ id.
+     *
+     * @ORM\Id
+     * @ORM\ManyToOne(targetEntity="Organ")
+     */
+    protected $organ;
+
+    /**
+     * Member lidnr.
+     *
+     * @ORM\Id
+     * @ORM\ManyToOne(targetEntity="Member")
+     * @ORM\JoinColumn(name="lidnr",referencedColumnName="lidnr")
+     */
+    protected $lidnr;
+
+    /**
+     * Function.
+     *
+     * @ORM\Id
+     * @ORM\Column(type="string")
+     */
+    protected $function;
+
+    /**
+     * Installation date.
+     *
+     * @ORM\Id
+     * @ORM\Column(type="date")
+     */
+    protected $installDate;
+
+    /**
+     * Installation.
+     *
+     * @ORM\OneToOne(targetEntity="Report\Model\SubDecision\Installation")
+     * @ORM\JoinColumns({
+     *  @ORM\JoinColumn(name="r_meeting_type", referencedColumnName="meeting_type"),
+     *  @ORM\JoinColumn(name="r_meeting_number", referencedColumnName="meeting_number"),
+     *  @ORM\JoinColumn(name="r_decision_point", referencedColumnName="decision_point"),
+     *  @ORM\JoinColumn(name="r_decision_number", referencedColumnName="decision_number"),
+     *  @ORM\JoinColumn(name="r_number", referencedColumnName="number")
+     * })
+     */
+    protected $installation;
+
+    /**
+     * Discharge date.
+     *
+     * @ORM\Column(type="date")
+     */
+    protected $dischargeDate;
+}
