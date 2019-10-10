fd0013cc6cf2205c372b52fb5e19c9bdcbda6db0
Pieter Kokx <pieter@kokx.nl>
2019-10-10 20:43:09 +0200

    Created user module

---
+++ module/User/Module.php
@@ -0,0 +1,16 @@
+<?php
+namespace USer;
+
+class Module
+{
+
+    /**
+     * Get the configuration for this module.
+     *
+     * @return array Module configuration
+     */
+    public function getConfig()
+    {
+        return include __DIR__ . '/config/module.config.php';
+    }
+}
