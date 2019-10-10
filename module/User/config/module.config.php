5718840a34e3020db7a8bfd075db49ce7848fe0c
Pieter Kokx <pieter@kokx.nl>
2019-10-10 22:02:55 +0200

    Added basic controller

--- module/User/config/module.config.php
+++ module/User/config/module.config.php
@@ -1,2 +1,24 @@
 <?php
-return [];
+use User\Controller\UserController;
+
+return [
+    'router' => [
+        'routers' => [
+            'user' => [
+                'type' => 'literal',
+                'options' => [
+                    'route' => '/user',
+                    'defaults' => [
+                        'controller' => UserController::class,
+                        'action' => 'index'
+                    ]
+                ]
+            ]
+        ]
+    ],
+    'controllers' => [
+        'invokables' => [
+            UserController::class => UserController::class
+        ]
+    ]
+];
---
+++ module/User/src/Controller/UserController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace User\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+
+class UserController extends AbstractActionController
+{
+
+    /**
+     * User login action
+     */
+    public function indexAction()
+    {
+        var_dump("Whatever.");
+        exit;
+    }
+}
