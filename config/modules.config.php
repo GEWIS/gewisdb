67fff6687acbcdf3308e2b29a395bffcdaca2f05
Koen1999 <koen.teuwen@gmail.com>
2022-06-12 13:08:50 +0200

    Dockerized gewisdb

---
+++ config/modules.config.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * List of enabled modules for this application.
+ *
+ * This should be an array of module namespaces used in the application.
+ */
+
+return [
+    'DoctrineModule',
+    'DoctrineORMModule',
+    'Application',
+    'Database',
+    'Import',
+    'Checker',
+    'Export',
+    'Report',
+    'User',
+];
