b9673115aad9f94106ff9397ed0d93be2bb9775d
Koen1999 <koen.teuwen@gmail.com>
2022-06-15 18:47:51 +0200

    Fixed repo naming in replenish script

--- replenish.sh
+++ replenish.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
-export REPOSITORY="GEWIS/gewisweb"
+export REPOSITORY="GEWIS/gewisdb"
 export BRANCH="master"
 cd /tmp
 apt-get update && apt-get install -y wget
 wget --no-cache "https://github.com/${REPOSITORY}/archive/refs/heads/${BRANCH}.zip"
 unzip "${BRANCH}.zip"
 rm "${BRANCH}.zip"
-cp -R -u gewisweb-${BRANCH}/public/* /code/public/
+cp -R -u gewisdb-${BRANCH}/public/* /code/public/
 chown -R  www-data:www-data /code/public
-cp -R -u gewisweb-${BRANCH}/data/* /code/data/
+cp -R -u gewisdb-${BRANCH}/data/* /code/data/
 chown -R  www-data:www-data /code/data
-rm -R /tmp/gewisweb-${BRANCH}
+rm -R /tmp/gewisdb-${BRANCH}
 cd /code
 if [ "${APP_ENV}" == 'production' ]
 then
