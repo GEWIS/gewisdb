201d4a3dd9aab554869d51ef7f4890e9f0168f4d
Tom Udding <tomudding@users.noreply.github.com>
2022-08-15 17:38:41 +0200

    Fix cron not running

--- docker/web/development/docker-entrypoint.sh
+++ docker/web/development/docker-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 printenv | sed 's/^\(.*\)$/export \1/g' | grep -E "^export (APP|CHECKER|DOCTRINE|SMTP)_" > ./config/bash.env
-service cron start
+crond -b
 ./orm orm:generate-proxies
 EM_ALIAS=orm_report ./orm orm:generate-proxies
 php-fpm -F -O
