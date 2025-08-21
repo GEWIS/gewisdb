79a970e73621451036680fcd1bd5611598a2f8e9
rinkp <rink@gewis.nl>
2025-08-21 14:04:55 +0200

    feat(user): Introduce created/updated date for users/api principals

---
+++ module/Database/migrations/Version20250821111612.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Database\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * phpcs:disable Generic.Files.LineLength.TooLong
+ * phpcs:disable SlevomatCodingStandard.Functions.RequireMultiLineCall.RequiredMultiLineCall
+ */
+final class Version20250821111612 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'Keep track of creation and modification dates of users and api principals';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $this->addSql('ALTER TABLE apiprincipal ADD createdAt TIMESTAMP(0) WITHOUT TIME ZONE, ADD updatedAt TIMESTAMP(0) WITHOUT TIME ZONE');
+        $this->addSql('UPDATE apiprincipal set createdAt = \'1900-01-01\', updatedAt = \'1900-01-01\'');
+        $this->addSql('ALTER TABLE apiprincipal ALTER COLUMN createdAt SET NOT NULL, ALTER COLUMN updatedAt SET NOT NULL;');
+
+        $this->addSql('ALTER TABLE users ADD createdAt TIMESTAMP(0) WITHOUT TIME ZONE, ADD updatedAt TIMESTAMP(0) WITHOUT TIME ZONE');
+        $this->addSql('UPDATE users set createdAt = \'1900-01-01\', updatedAt = \'1900-01-01\'');
+        $this->addSql('ALTER TABLE users ALTER COLUMN createdAt SET NOT NULL, ALTER COLUMN updatedAt SET NOT NULL;');
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->addSql('ALTER TABLE users DROP createdAt, DROP updatedAt');
+        $this->addSql('ALTER TABLE ApiPrincipal DROP createdAt, DROP updatedAt');
+    }
+}
