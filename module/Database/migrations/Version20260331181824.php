c126978b88fb7b2ae4506b46159c0e6f1a5ee1a6
Victor Klomp <victor@gewis.nl>
2026-05-21 16:50:41 +0200

    fix: remove excess in migration

--- module/Database/migrations/Version20260331181824.php
+++ module/Database/migrations/Version20260331181824.php
@@ -20,9 +20,7 @@ final class Version20260331181824 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE ListmonkMailingList (id VARCHAR(255) NOT NULL, name VARCHAR(255) NOT NULL, lastSeen TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, lastCheck TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, PRIMARY KEY(id))');
-        $this->addSql('ALTER TABLE configitem ALTER version SET DEFAULT 1');
         $this->addSql('ALTER TABLE mailinglist ADD listmonkId VARCHAR(255) DEFAULT NULL');
         $this->addSql('ALTER TABLE mailinglist ADD CONSTRAINT FK_FD864C3AB97ED0D8 FOREIGN KEY (listmonkId) REFERENCES ListmonkMailingList (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
         $this->addSql('CREATE UNIQUE INDEX UNIQ_FD864C3AB97ED0D8 ON mailinglist (listmonkId)');
@@ -30,10 +28,8 @@ final class Version20260331181824 extends AbstractMigration
 
     public function down(Schema $schema): void
     {
-        // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE MailingList DROP CONSTRAINT FK_FD864C3AB97ED0D8');
         $this->addSql('DROP TABLE ListmonkMailingList');
-        $this->addSql('ALTER TABLE ConfigItem ALTER version SET DEFAULT 1000');
         $this->addSql('DROP INDEX UNIQ_FD864C3AB97ED0D8');
         $this->addSql('ALTER TABLE MailingList DROP listmonkId');
     }
