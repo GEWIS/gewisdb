25d1bf6ae60573e07b7e15cc277111131c517dbc
Victor Klomp <victor@gewis.nl>
2026-05-27 12:50:58 +0200

    chore: re-create migration

--- module/Database/migrations/Version20260527104946.php
+++ module/Database/migrations/Version20260527104946.php
@@ -8,10 +8,9 @@ use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
 /**
- * phpcs:disable Generic.Files.LineLength.TooLong
- * phpcs:disable SlevomatCodingStandard.Functions.RequireMultiLineCall.RequiredMultiLineCall
+ * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20260331181824 extends AbstractMigration
+final class Version20260527104946 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,8 +19,8 @@ final class Version20260331181824 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        $this->addSql('CREATE TABLE ListmonkMailingList (id VARCHAR(255) NOT NULL, name VARCHAR(255) NOT NULL, lastSeen TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, lastCheck TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, PRIMARY KEY(id))');
-        $this->addSql('ALTER TABLE mailinglist ADD listmonkId VARCHAR(255) DEFAULT NULL');
+        $this->addSql('CREATE TABLE ListmonkMailingList (id INT NOT NULL, name VARCHAR(255) NOT NULL, lastSeen TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, lastCheck TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, PRIMARY KEY(id))');
+        $this->addSql('ALTER TABLE mailinglist ADD listmonkId INT DEFAULT NULL');
         $this->addSql('ALTER TABLE mailinglist ADD CONSTRAINT FK_FD864C3AB97ED0D8 FOREIGN KEY (listmonkId) REFERENCES ListmonkMailingList (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
         $this->addSql('CREATE UNIQUE INDEX UNIQ_FD864C3AB97ED0D8 ON mailinglist (listmonkId)');
     }
